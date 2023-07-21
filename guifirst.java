62a382cb97fd88f1291090c1951cdc45cce5e175
Akshita Das <75940178+akshita1809@users.noreply.github.com>
2023-07-21 15:19:27 +0530

    Update guifirst.java

--- guifirst.java
+++ guifirst.java
@@ -3,8 +3,8 @@ public class test{
     public static void main(String[] args) {
         String name= JOptionPane.showInputDialog("enter your name:"); //to generate input dialouge box
         JOptionPane.showMessageDialog(null, "hello "+ name); //takes input from user and generates personalised response
-        int height= Integer.parseInt( JOptionPane.showInputDialog("enter your height in cm:"));
-        JOptionPane.showMessageDialog(null, "you are "+ height+ "cm tall");
+        int height= Integer.parseInt( JOptionPane.showInputDialog("enter your height in cm:")); //takes input of height from user
+        JOptionPane.showMessageDialog(null, "you are "+ height+ "cm tall"); //output=height
         double age= Double.parseDouble( JOptionPane.showInputDialog("enter your height in cm:"));
         JOptionPane.showMessageDialog(null, "you are "+ age+ "years old");
     }
